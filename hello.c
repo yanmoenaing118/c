0d4d8057073dfdb1f08c4b78508296d99a43a68f
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-07 22:06:07 +0630

    EOF

--- hello.c
+++ hello.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
 int main() {
+	int c;
 
-	float fahr, celsius;
-	float lower, upper, step;
-
-	lower = 0;
-	upper = 150;
-	step = 5;
-
-	celsius = lower;
-
-	printf("Celsius\tFahrenheit\n");
-	while (celsius <= upper)
-	{
-		fahr = (celsius * 9.0) / 5.0 - 32;
-		printf("%7.0f\t%10.1f\n", fahr, celsius);
-		celsius = celsius + step;
+	if(EOF == -1) {
+		printf("EOF is -1\n");
 	}
-	
 
+	c = getchar() == EOF;
+
+	if(c ==0) {
+		printf("This is not EOF %d\n", c);
+	}
+	if(c == 1) {
+		printf("This is EOF %d\n", c);
+	}
 	return 0;
 }
 
