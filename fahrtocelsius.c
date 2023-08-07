52659f87df00060223e66e78e8a37feec84a73f6
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-07 21:36:31 +0630

    #define

--- fahrtocelsius.c
+++ fahrtocelsius.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
 
 	float fahr, celsius;
-	float lower, upper, step;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
+	fahr = LOWER;
 
 	printf("Fahrenheit\tCelsius\n");
-	for (fahr = upper; fahr >= lower; fahr -= step)
+	for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
 		printf("%10.0f\t%7.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
 	return 0;
