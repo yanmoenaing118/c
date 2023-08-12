c613dbac39fe9c64729d62f6a8262c0d9225bed0
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-12 22:37:59 +0630

    histogram

--- e1-14.c
+++ e1-14.c
@@ -9,8 +9,9 @@ int main()
     int nchars[TOTAL_ASCII_CHARS];
     int longest;
     int chars;
+    int longest_copy;
 
-    longest = chars = 0;
+    longest = longest_copy = chars = 0;
 
     for (int i = 0; i < TOTAL_ASCII_CHARS; i++)
         nchars[i] = 0;
@@ -21,13 +22,22 @@ int main()
         nchars[ch]++;
     }
 
+    printf("Horizontal histogram\n");
+
     for (int i = 0; i < TOTAL_ASCII_CHARS; i++)
     {
         if ((char)i == '\n')
             continue;
         if (nchars[i] == 0)
             continue;
+
         printf("%c ", i);
+
+        chars++;
+
+        if (nchars[i] > longest)
+            longest = nchars[i];
+
         for (int j = 0; j < nchars[i]; j++)
         {
             putchar('#');
@@ -35,6 +45,36 @@ int main()
         printf("\n");
     }
 
+    longest_copy = longest;
+
+    printf("Vertical histogram\n");
+    for (int i = 0; i < longest; i++)
+    {
+        for (int j = 0; j < TOTAL_ASCII_CHARS; j++)
+        {
+            if (nchars[j] == 0 || (char)j == '\n')
+                continue;
+            if (longest_copy - nchars[j] <= 0)
+            {
+                putchar('#');
+            }
+            else
+            {
+                putchar(' ');
+            }
+        }
+        if (i == longest - 1)
+        {
+            putchar('\n');
+            for( int j = 0 ; j < TOTAL_ASCII_CHARS; j++)
+            {
+                if(nchars[j] == 0 || (char)j == '\n') continue;
+                putchar((char)j);
+            }
+        }
+        longest_copy--;
+        putchar('\n');
+    }
 
     return 0;
 }
