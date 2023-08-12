ae5d137e7fad414ec40b25f7ac4d75c246e4e86f
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-12 21:06:31 +0630

    save

--- e1-13.c
+++ e1-13.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT -1
+
+/**
+ *
+ * my version of word count program
+ */
+
 int main()
 {
-    
+    int ch;
+
+    int STATE = OUT;
+
+    unsigned long wc = 0;
+
+    while ((ch = getchar()) != EOF)
+    {
+        if (ch != '\n' && ch != ' ' && ch != '\t')
+        {
+            if (STATE == OUT)
+                ++wc;
+            STATE = IN;
+            putchar('#');
+        }
+        else
+        {
+            STATE = OUT;
+            putchar('\n');
+        }
+    }
 
     return 0;
 }
