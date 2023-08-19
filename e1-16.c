ed2fa42198837f2d477033973e33be0e31caf37c
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-19 15:02:39 +0630

    save

---
+++ e1-16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 1000
+
+int get_line(char line[], int limit);
+void copy(char from[], char to[]);
+
+int main()
+{
+    int len;
+    int max;
+    char longest[MAX_LENGTH];
+    char line[MAX_LENGTH];
+
+    
+
+
+
+    
+    return 0;
+
+}
+
+int get_line(char line[], int limit)
+{
+
+
+
+
+}
