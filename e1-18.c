cb7bfb70b74a594587e41804deb1434f1f1ef0e5
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-22 21:19:42 +0630

    e1-18.c

--- e1-18.c
+++ e1-18.c
@@ -12,7 +12,7 @@ int main()
     char line[MAX_LINE];
 
     while((len = get_line(line, MAX_LINE)) > 0 ) {
-        printf("%s", line);
+        printf("%s %d\n", line, len);
     }
     
     return 0;
@@ -20,7 +20,35 @@ int main()
 
 
 int get_line(char line[], int limit) {
+    int i = 0;
+    int j = 0;
+    int c;
+    for(i = 0 ; i < limit - 1 && (c = getchar()) != '\n' && c != EOF; i++ ) {
+        line[i] = c;
+    }
 
+    j = i;
+
+    while (line[j - 1] == '\t' || line[j - 1] == ' ')
+    {
+        j--;
+    }
+
+    if(line[i - 1] != '\t' && line[i - 1] != ' ') {
+        if(c == '\n') {
+            line[i] = c;
+            i++;
+        }
+        line[i] = '\0';
+    } else {
+        i = j;
+
+        line[++i] = '\n';
+        line[++i] = '\0';
+    }
+    
+    return i;
+    
 }
 
 
