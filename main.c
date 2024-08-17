1f3630cb42cea195f2cc95f57762374f81e60a6d
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-08-17 07:13:44 +0630

    Restart

--- main.c
+++ main.c
@@ -1,37 +1,10 @@
 #include <stdio.h>
 
-void remove_comments() {
-    int c, next;
+int main()
+{
 
-    while ((c = getchar()) != EOF) {
-        if (c == '/') {
-            next = getchar();
-
-            if (next == '*') {
-                // Multi-line comment found, skip until the end of the comment
-                while ((c = getchar()) != '*' || (next = getchar()) != '/')
-                    if (next == EOF) break;
-            } else if (next == '/') {
-                // Single-line comment found, skip until the end of the line
-                while ((c = getchar()) != '\n')
-                    if (c == EOF) break;
-
-                putchar('\n');  // Add a newline after single-line comment removal
-            } else {
-                putchar(c);
-                // ungetc(next, stdin);
-            }
-        } else {
-            putchar(c);
-        }
-    }
-}
-
-int main() {
-    // printf("Enter C code (Ctrl+D to stop):\n");
-    remove_comments();
-
-    // printf("Comments removed successfully.\n");
-
+    printf("Hello World.\n What is your name?");
     return 0;
 }
+
+
