f71ba569feba7224d2e9d86cfcb1b29b9019b805
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-08-18 08:22:28 +0630

    Exercise 1-4

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
 {
-    
-    for(int F = 0; F <= 300; F += 20 ) {
-        int C =( 5  * ( F - 32) ) / 9;
-        printf("%d\t%d\n", F, C );
-    }
     
     return 0;
 }
